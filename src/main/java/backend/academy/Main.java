4174af38645a06095d8bdfd10500601cbcd431e8
Богдан Лямзин <byl1@tpu.ru>
2024-11-18 03:51:24 +0700

    chore: добавлен запуск в main

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,16 @@
 package backend.academy;
 
+import backend.academy.log.analyzer.app.AnalyzerApplication;
+import backend.academy.log.analyzer.config.LogAnalyzerModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new LogAnalyzerModule());
+        AnalyzerApplication app = injector.getInstance(AnalyzerApplication.class);
+        app.run(args);
     }
 }
